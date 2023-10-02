89fc401941b62976eeb1f06596490c45ac076b6f
jimklo83 <jimklo83@mail.com>
2023-10-02 14:53:17 -0500

    Added loop

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,25 +4,51 @@
     {
         static void Main(string[] args)
         {
-            var random = new Random();
-            var favNumber = random.Next(1, 1000);
-            Console.Write("Guess what my favorite number is:");
-            var userInput = int.Parse(Console.ReadLine());
+            Console.Write("Do you want to play a guessing game?");
+            string playGame = Console.ReadLine();
 
-            if (userInput < favNumber)
+            while (playGame != "yes" && playGame != "no")
             {
-                Console.WriteLine("Too low...");
+                Console.WriteLine("That was not an option.\nDo you want to play a guessing game? ('yes' or 'no')");
+                playGame = Console.ReadLine();
             }
 
-            else if (userInput > favNumber)
+            while (playGame == "yes")
             {
-                Console.WriteLine("Too high...");
-            }
+                var random = new Random();
+                var favNumber = random.Next(1, 1000);
+                var userInput = 0;
 
-            else
-            {
-                Console.WriteLine("You got it!!!");
+                while (userInput != favNumber) 
+                {
+                    Console.Write("Guess what my favorite number is:");
+                    userInput = int.Parse(Console.ReadLine());
+                    if (userInput < favNumber)
+                    {
+                        Console.WriteLine($"Too low...\nYour guess: {userInput}");
+                    }
+
+                    else if (userInput > favNumber)
+                    {
+                        Console.WriteLine($"Too high...\nYour guess: {userInput}");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"You got it!!!\nYour guess: {userInput}");
+                    }
+                }
+
+                Console.WriteLine("Do you want to play again?");
+                playGame = Console.ReadLine();
             }
+            
+            
+            
+           
+            
+
+            
         }
     }
 }
