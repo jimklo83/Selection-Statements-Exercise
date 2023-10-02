07ee213cac9db8ffb96223e03db3676a2bd4209b
jimklo83 <jimklo83@mail.com>
2023-10-02 13:18:05 -0500

    Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var random = new Random();
+            var favNumber = random.Next(1, 1000);
+            Console.Write("Guess what my favorite number is:");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low...");
+            }
+
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high...");
+            }
+
+            else
+            {
+                Console.WriteLine("You got it!!!");
+            }
         }
     }
 }
